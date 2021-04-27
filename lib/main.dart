390f9743b2589728d5fe3bc2d9167b5e0370569d
Jemima <contatojemima@gmail.com>
2021-04-27 04:06:22 -0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      theme: lightThemeData(context),
+      theme: darkThemeData(context),
       darkTheme: darkThemeData(context),
       home: WelcomeScreen(),
     );
